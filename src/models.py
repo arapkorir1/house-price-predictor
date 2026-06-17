87bcf2c3de8b02f7db2ed6c9747b716c8ef92d45
Victor <Victor.K100@proton.me>
2026-06-17 23:30:56 +0300

    refactor: tidy up functions by removing uneccesary code

--- src/models.py
+++ src/models.py
@@ -1,27 +1,38 @@
 from config import RANDOM_SEED
 
-def get_baseline_models():
-    '''
-    return baseline models
-    '''
-    from sklearn.dummy import DummyRegressor
-    from sklearn.linear_model import LinearRegression, Ridge 
-    from sklearn.ensemble import RandomForestRegressor
+def get_baseline_model():
 
+    from sklearn.dummy import DummyRegressor
+    from sklearn.linear_models import LInearRegressor,Ridge
+    from sklearn.ensemble import RandomForestRegressor
+    
     return{
-            'dummy':DummyRegressor(strategy='mean'),
+            'dummy': DummyRegressor(strategy='mean'),
             'linear': LinearRegression(),
-            'ridge':Ridge(alpha=1.0),
-            'randomforest':RandomForestRegressor(
-                n_estimators=100,
-                n_jobs=1,
-                max_depth=10,
+            'ridge': RidgeRegressor(alpha=1),
+            'randomforest': RandomForestFRegressor(
+                n_estimators=500,
+                max_depth=6,
+                n_jobs=1, 
                 random_state=RANDOM_SEED
                 )
             }
-'''
-regularization is highly sensitive to the scale of  input features; scale for ridge regression
-'''
 
-def get_xgboost(task='regression'):
-     from sklearn import 
+def get_xgboost(**kwargs):
+
+    import xgboost as xgb
+
+    defaults= {
+            'n_estimators': 500,
+            'max_depth': 6,
+            'learning_curve': 0.05,
+            'random_state': RANDOM_SEED
+            'n-jobs': 1
+            }
+    defaults.update(kwargs)
+
+    return xgb.XGBRegressor(**defaults)
+
+def 
+
+
